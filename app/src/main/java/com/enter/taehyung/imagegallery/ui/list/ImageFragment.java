0924181eb5037d0461e8fd41536ab8e69ca51434
TaeHyungK <xogud3125@naver.com>
2020-04-26 16:40:54 +0900

    #1 [Bug] Staggered 형태 노출 후 빠르게 스크롤 시 균형 맞지않는 현상 - GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS 옵션으로 변경

--- app/src/main/java/com/enter/taehyung/imagegallery/ui/list/ImageFragment.java
+++ app/src/main/java/com/enter/taehyung/imagegallery/ui/list/ImageFragment.java
@@ -80,7 +80,7 @@ public class ImageFragment extends Fragment implements ImageContract.View{
 
         mGridLayoutManager = new GridLayoutManager(getActivity(), ImageConst.SPAN_COUNT.THREE, RecyclerView.VERTICAL, false);
         mStaggeredLayoutManager= new StaggeredGridLayoutManager(ImageConst.SPAN_COUNT.THREE, RecyclerView.VERTICAL);
-        mStaggeredLayoutManager.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_NONE);
+        mStaggeredLayoutManager.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS);
 
         mAdapter = new ImageAdapter(mPresenter, imageList, ImageConst.LAYOUT_TYPE.DEFAULT);
         mAdapter.setListener(mClickListener);
